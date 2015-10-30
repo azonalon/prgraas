b03ff13b3fee7b1c9b5a6f36a3b42843a09769d5
azonalon <sauter.eduard@gmail.com>
2015-10-30 14:16:08 +0100

    removed length typedef

--- assignment02/truncate_to_n.c
+++ assignment02/truncate_to_n.c
@@ -5,17 +5,16 @@ Run: truncate_to_n
 
 #include "base.h"
 #include "string.h"
- 
+
 /*
 Design a function that cuts strings to length n and truncates the rest of the
 string.
 */
- 
-typedef int Length;             // the length of the output
- 
+
+
 // (String, int) -> String
-String truncate_to_n(String input, Length n);
- 
+String truncate_to_n(String input, int length);
+
 void truncate_to_n_test() {
     check_expect_s(truncate_to_n("a", 2), "a");              // no truncation
     check_expect_s(truncate_to_n("ab", 2), "ab");            // no truncation
@@ -24,15 +23,15 @@ void truncate_to_n_test() {
     check_expect_s(truncate_to_n("blabla", 6), "blabla");    // no truncation
     check_expect_s(truncate_to_n("blablabla", 6), "blabla"); // truncation
 }
- 
+
 /* Return a truncated string of length n
 given a string of arbitrary length and n.
 */
-String truncate_to_n(String input, Length n) {
-    if (s_length(input) <= n) {
+String truncate_to_n(String input, int length) {
+    if (s_length(input) <= length) {
         return input;
     } else {
-        return s_sub(input, 0, n);
+        return s_sub(input, 0, length);
     }
 }
 
