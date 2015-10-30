a1d96da668e21fe43baf1a3f8723149db96a7d3a
azonalon <sauter.eduard@gmail.com>
2015-10-30 07:07:30 +0100

    remove references to trunactedstring

--- assignment02/truncate_to_8.c
+++ assignment02/truncate_to_8.c
@@ -5,24 +5,23 @@ Run: truncate_to_8
 
 #include "base.h"
 #include "string.h"
- 
+
 /*
 Design a function that cuts strings to length 8 and truncates the rest of the
 string.
 */
- 
+
 String truncate_to_8(String);
-/* typedef String TruncatedString; // the output of the function */
- 
-// String -> TruncatedString
+
+// String -> String
 /* TruncatedString truncate_to_8(String input); */
- 
+
 void truncate_to_8_test() {
     check_expect_s(truncate_to_8("bla"), "bla");            // no truncation
     check_expect_s(truncate_to_8("blablabl"), "blablabl");  // no truncation
     check_expect_s(truncate_to_8("blablabla"), "blablabl"); // truncation
 }
- 
+
 // Return a truncated string of length 8 given a string of arbitrary length.
 String truncate_to_8(String input) {
     if (s_length(input) <= 8) {
