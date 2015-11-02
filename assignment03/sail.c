e7fe893e5012461be82c1ef9e738b37b0cc1b64e
qwertzuiop961 <felix.schrader@gmx.net>
2015-11-02 14:11:21 +0100

    aufgabe 3

--- assignment03/sail.c
+++ assignment03/sail.c
@@ -6,36 +6,74 @@ Run: ./sail
 #include "base.h"
 
 /*
-In einer Spielewelt sei die Erde eine Scheibe mit den Kontinenten Europa, Afrika und Amerika. Afrika liegt südlich von Europa. Amerika liegt westlich von Europa und Afrika. In der Spielewelt können Nord-, Süd-, Ost- und Westwinde auftreten. In der Spielewelt gibt es genau ein Segelschiff. Wenn das Segelschiff bei Westwind in Europa startet, fährt es nach Amerika. Wenn es bei Ostwind in Amerika startet, landet es (auf Grund einer nördlichen Wasserströmung) in Europa. Wenn es bei Westwind von Amerika aus startet, fällt es ins Nichts. Wenn es bei Südwind in Europa startet, landet es in Afrika. Entsprechendes gilt für die anderen möglichen Kombinationen von Startposition und Windrichtung. Nehmen Sie an, dass sich die Windrichtung nur vor, aber nie während einer Reise des Segelschiffs ändert. Entwickeln Sie eine Funktion, die abhängig von der Startposition des Segelschiffs und der Windrichtung beim Start die Zielposition bestimmt.
+In einer Spielewelt sei die Erde eine Scheibe mit den Kontinenten Europa,
+Afrika und Amerika. Afrika liegt südlich von Europa. Amerika liegt westlich von
+Europa und Afrika. In der Spielewelt können Nord-, Süd-, Ost- und Westwinde
+auftreten. In der Spielewelt gibt es genau ein Segelschiff. Wenn das
+Segelschiff bei Westwind in Europa startet, fährt es nach Amerika. Wenn es bei
+Ostwind in Amerika startet, landet es (auf Grund einer nördlichen
+Wasserströmung) in Europa. Wenn es bei Westwind von Amerika aus startet, fällt
+es ins Nichts. Wenn es bei Südwind in Europa startet, landet es in Afrika.
+Entsprechendes gilt für die anderen möglichen Kombinationen von Startposition
+und Windrichtung. Nehmen Sie an, dass sich die Windrichtung nur vor, aber nie
+während einer Reise des Segelschiffs ändert. Entwickeln Sie eine Funktion, die
+abhängig von der Startposition des Segelschiffs und der Windrichtung beim Start
+die Zielposition bestimmt.
 */
 
-enum TrafficLight {
-    RED,
-    GREEN,
-    YELLOW
+enum Continent {
+    AFRICA,
+    AMERICA,
+    EUROPE,
+    NOWHERE
 };
 
-// enum TrafficLight -> enum TrafficLight
-// Return the amount of tax for the given price.
-enum TrafficLight sail(enum TrafficLight);
+enum WindDiretion {
+    NORTH,
+    WEST,
+    EAST,
+    SOUTH
+};
+
+// (enum Continent, enum WindDiretion) -> enum Continent
+// Return the next continent for a given continent and a wind direction.
+enum Continent sail(enum Continent starting_continent, enum WindDiretion wd);
 
 static void sail_test() {
-    check_expect_i(sail(RED), GREEN);
-    check_expect_i(sail(GREEN), YELLOW);
-    check_expect_i(sail(YELLOW), RED);
+    check_expect_i(sail(AMERICA, EAST), EUROPE);
+    check_expect_i(sail(EUROPE, WEST), AMERICA);
+    check_expect_i(sail(EUROPE, SOUTH), AFRICA);
+    check_expect_i(sail(AFRICA, NORTH), EUROPE);
+    check_expect_i(sail(AMERICA, SOUTH), NOWHERE);
+    check_expect_i(sail(AFRICA, EAST), NOWHERE);
+    check_expect_i(sail(NOWHERE, NORTH), NOWHERE);
+    check_expect_i(sail(NOWHERE, WEST), NOWHERE);
 }
 
-// Produces the next color of a traffic light
-// given the current color of the traffic light.
-enum TrafficLight sail(enum TrafficLight tl) {
-    if (tl == RED) {
-        return GREEN;
-    } else if (tl == GREEN) {
-        return YELLOW;
-    } else if (tl == YELLOW) {
-        return RED;
+enum Continent sail(enum Continent starting_continent, enum WindDiretion wd) {
+    if (starting_continent == AFRICA) {
+        if (wd == NORTH) {
+            return EUROPE;
+        } else {
+            return NOWHERE;
+        }
+    } else if (starting_continent == AMERICA) {
+        if (wd == EAST) {
+            return EUROPE;
+        } else {
+            return NOWHERE;
+        }
+    } else if (starting_continent == EUROPE) {
+        if (wd == WEST) {
+            return AMERICA;
+        } else if (wd == SOUTH) {
+            return AFRICA;
+        } else {
+            return NOWHERE;
+        }
+    } else {
+        return NOWHERE;
     }
-    return RED;
 }
 
 int main(void) {
