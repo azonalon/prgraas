80e4951d102e0a33b71991b8aa18b1aab7184203
azonalon <sauter.eduard@gmail.com>
2015-11-17 13:32:33 +0100

    finish turtle

--- assignment05/turtle.c
+++ assignment05/turtle.c
@@ -2,7 +2,7 @@
 Compile: make turtle_solution
 Run: ./turtle_solution
 
-*** Turtle Command Reference *** 
+*** Turtle Command Reference ***
 FORWARD <VALUE>                // turtle walks <value> pixel forward
 RIGHT <DEGREE>                 // turtle turns right by <degree> degrees
 BACKWARD <VALUE>
@@ -19,8 +19,9 @@ PENDOWN                        // turtle starts drawing when it moves
 #include "base.h"
 #include "string.h"
 
-const int WIDTH = 200;
-const int HEIGHT = 200;
+#define WIDTH  200
+#define HEIGHT  200
+#define M_PI 3.14159265358979323846
 unsigned char image[WIDTH * HEIGHT];
 
 enum PenState { UP, DOWN };
@@ -40,9 +41,9 @@ void set_pixel(int x, int y, int color) {
 // adapted from http://rosettacode.org/wiki/Bitmap/Bresenham's_line_algorithm#C
 void draw_line(int x0, int y0, int x1, int y1) {
 	int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-	int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1; 
+	int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
 	int err = (dx > dy ? dx : -dy) / 2, e2;
-	
+
 	while (true) {
 		set_pixel(x0, y0, 0);
 		if(x0 == x1 && y0 == y1) break;
@@ -52,7 +53,7 @@ void draw_line(int x0, int y0, int x1, int y1) {
 			x0 += sx;
 		}
 		if(e2 < dy){
-			err += dx; 
+			err += dx;
 			y0 += sy;
 		}
 	}
@@ -69,11 +70,11 @@ void clear(void) {
 void move_turtle(int movement) {
 	int dx = (int)(movement * cos(turtle_dir * M_PI / 180.0));
 	int dy = (int)(movement * sin(turtle_dir * M_PI / 180.0));
-	
+
 	if (pen_state == DOWN) {
 		draw_line(turtle_x, turtle_y, turtle_x + dx, turtle_y + dy);
 	}
-	
+
 	turtle_x += dx;
 	turtle_y += dy;
 }
@@ -82,21 +83,21 @@ void parse_command(String command) {
 	if (s_equals(command, "CLEARSCREEN")) {
 		clear();
 	} else if(s_equals(command, "PENUP")) {
-		pen_state = ...?;
+		pen_state = UP;
 	} else if(s_equals(command, "PENDOWN")) {
-		pen_state = ...?;
+		pen_state = DOWN;
 	} else if(s_equals(command, "FORWARD")) {
 		int movement = i_input();
 		move_turtle(movement);
 	} else if(s_equals(command, "BACKWARD")) {
-		...?
-		...?
+		int movement = -i_input();
+		move_turtle(movement);
 	} else if(s_equals(command, "LEFT")) {
-		...?
-		turtle_dir = ...?;
+		int angle = -i_input();
+		turtle_dir = angle;
 	} else if(s_equals(command, "RIGHT")) {
-		...?
-		turtle_dir = ...?;
+		int angle = i_input();
+		turtle_dir = angle;
 	} else {
 		printf("Unknown command: %s\n", command);
 	}
@@ -104,12 +105,12 @@ void parse_command(String command) {
 
 int main(void) {
 	clear();
-	
+
 	String command = s_input(100);
 	while (!s_equals(command, "")) {
 		parse_command(command);
 		command = s_input(100);
 	}
-	
+
 	stbi_write_png("result.png", WIDTH, HEIGHT, 1, image, WIDTH);
 }
