f51c180ef1fd4c27d0e1aab09a74c1f13b37d360
azonalon <sauter.eduard@gmail.com>
2015-12-01 13:05:54 +0100

    quicksort visualization finish

--- assignment07/quicksort_visualization.c
+++ assignment07/quicksort_visualization.c
@@ -47,7 +47,15 @@ void write_image(Image *image, int index) {
 }
 
 void set_pixel(Image *image, int x, int y, Color color) {
-	// todo: implement (a)
+    assert(x < image->width);
+    assert(y < image->height);
+
+    int h = image->height - y;
+    Byte* first = image->data + 3 * image->width * h + 3 * x;
+    *(first) = color.red;
+    *(first + 1) = color.green;
+    *(first + 2) = color.blue;
+
 }
 
 void swap(int v[], int i, int j) {
@@ -55,33 +63,62 @@ void swap(int v[], int i, int j) {
 	v[i] = v[j];
 	v[j] = a;
 }
+Image quicksort_im;
+int quicksort_image_index = 1;
+const Color GREEN = {0, 255, 0};
+const Color RED = {255, 0, 0};
+const Color BLACK = {0, 0, 0};
+const Color WHITE = {255, 255, 255};
+
+void plot_array_64_black(Image* im, const int* data)
+{
+    clear_image(im);
+    for(int i = 0; i < 64; i++) {
+        set_pixel(im, i, data[i], BLACK);
+    }
+}
+
 
 void quicksort(int v[], int left, int right) {
+
+    plot_array_64_black(&quicksort_im, v);
 	if (left >= right) return; // 0 or 1 elements, recursion end
 	swap(v, left, (left + right) / 2); // move pivot element to left
 	int j = left;
 	for (int i = left + 1; i <= right; i++) {
-		if (v[i] < v[left]) {
+        if (v[i] < v[left]) {
 			swap(v, ++j, i);
+            set_pixel(&quicksort_im, i, v[i], RED);
+            set_pixel(&quicksort_im, j, v[j], RED);
 		}
 		// assert: v[i] < v[left] for i = left+1..j
 	}
 	swap(v, left, j); // move back pivot element
+    // mark the pivot
+    set_pixel(&quicksort_im, j, v[j], GREEN);
 
 	// todo: implement a visualization of the current state (c)
+    write_image(&quicksort_im, quicksort_image_index++);
 
 	quicksort(v, left, j-1); // assert: v[i] < v[j] for i = left..j-1
 	quicksort(v, j+1, right); // assert: v[i] >= v[j] for i = j+1..right
 }
 
+
 void quicksort_test(void) {
+    /* Image im = make_image(64, 64); */
+	int test_array[64];
+    for(int i = 0; i < 64; i++) {
+        test_array[i] = i_rnd(64);
+    }
+    plot_array_64_black(&quicksort_im, test_array);
+    write_image(&quicksort_im, 0);
 
-	// todo: create random array and create image that shows the initial state (b)
-
-	// todo: call quicksort(...);
-	
+    quicksort(test_array, 0, 64);
 }
 
 int main(void) {
+    quicksort_im = make_image(64, 64);
+    quicksort_image_index = 1;
 	quicksort_test();
 }
